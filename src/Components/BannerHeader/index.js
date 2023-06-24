1c89b1baba3830a3761e35a69b6cdd4454f18d3c
Gonzalo La Ferrera <charan@PC-cita>
2023-06-24 13:50:11 -0300

    Desarrollado y agregado el componente Button

--- src/Components/BannerHeader/index.js
+++ src/Components/BannerHeader/index.js
@@ -1,11 +1,12 @@
 import './BannerHeader.css';
+import Button from '../Button';
 
 function BannerHeader() {
     return <header className='header'>
         <div className='header__content'>
             <h1 className='header__titulo'>Promos de Invierno</h1>
             <h3 className='header__subTitulo'>40% de descuento</h3>
-            {/* acá va el botón */}
+            <Button>Ver Consolas</Button>
         </div>
 
     </header>
---
+++ src/Components/Button/index.js
@@ -0,0 +1,7 @@
+import "./Button.css";
+
+function Button(props) {
+    return <button className="boton">{props.children}</button>;
+}
+
+export default Button;
