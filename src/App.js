98eafbe12b662e7ea90f4f44ffd9f583787a2b21
Gonzalo La Ferrera <charan@PC-cita>
2023-06-28 12:04:00 -0300

    NavBar full desarrollado con Busqueda No Funcional / Plan A modificado por Plan B en JS y CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import BannerHeader from './Components/BannerHeader';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div>
+      <NavBar />
       <BannerHeader />
 
     </div>
---
+++ src/Components/NavBar/index.js
@@ -0,0 +1,23 @@
+import './NavBar.css';
+import Button from '../Button';
+
+function NavBar() {
+    return <div className='NavBar'>
+        <div className='NavBar__izquierda'>
+            <img className='NavBar__logoJoystick' src='/img/sports_esports_black_24dp 1.svg' alt='Icono del Logo'></img>
+            <img className='NavBar__logoTexto' src='/img/Texto.svg' alt='Texto del Logo'></img>
+            <img className='NavBar__barraBusqueda' src='/img/Barra_de_busqueda.png' alt='Img de barra de busqueda'></img>
+        </div>
+        
+        <Button>Login</Button>
+
+        {/* OPCION ORIGINAL */}
+        {/* <img className='NavBar__logoJoystick' src='/img/sports_esports_black_24dp 1.svg' alt='Icono del Logo'></img>
+        <img className='NavBar__logoTexto' src='/img/Texto.svg' alt='Texto del Logo'></img>
+        <img className='NavBar__barraBusqueda' src='/img/Barra_de_busqueda.png' alt='Img de barra de busqueda'></img>
+        <Button>Login</Button> */}
+        
+    </div>
+}
+
+export default NavBar;
